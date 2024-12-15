1c038fe4ad309338825a9e398c17c7835b458d7f
edidudedo <eduardoiglesius101@gmail.com>
2024-12-15 20:28:06 +0900

    Add User Reservation

---
+++ room-management/src/fetch/deleteReservationById.tsx
@@ -0,0 +1,25 @@
+const deleteRoomById = async (reservationId: number) => {
+    const baseUrl = "http://localhost:8080/api/reservations"; 
+    const url = `${baseUrl}/${reservationId}`; 
+  
+    try {
+        const response = await fetch(url, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+      
+          if (!response.ok) {
+            throw new Error(`Failed to delete reservation. Status: ${response.status}`);
+          }
+      
+          console.log(`Reservation ${reservationId} deleted successfully.`);
+        } catch (error) {
+          console.error("Error deleting reservation:", error);
+          throw error;
+        }
+    };
+  
+  export default deleteRoomById;
+  
